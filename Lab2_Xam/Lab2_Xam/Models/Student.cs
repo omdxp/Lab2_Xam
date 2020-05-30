5c6df2a0386a9816b0fd641c73a0694e558c69ad
Omar-Belghaouti <bel_omar18@yahoo.com>
2020-05-30 16:07:02 +0200

    Add project files.

---
+++ Lab2_Xam/Lab2_Xam/Models/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Lab2_Xam.Models
+{
+    class Student : INotifyPropertyChanged
+    {
+        private string name;
+        private string surname;
+        private int studentID;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public string Name 
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                if (surname != value)
+                {
+                    surname = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int StudentID
+        {
+            get { return studentID; }
+            set
+            {
+                if (studentID != value)
+                {
+                    studentID = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
---
+++ Lab2_Xam/Lab2_Xam/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Lab2_Xam.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Lab2_Xam.ViewModels
+{
+    class MainPageViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Student> Students { get; set; }
+
+        private Student selectedStudent;
+        public Student SelectedStudent
+        {
+            get
+            {
+                return selectedStudent;
+            }
+            set
+            {
+                if (selectedStudent != value)
+                {
+                    selectedStudent = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public MainPageViewModel()
+        {
+            Students = new ObservableCollection<Student>();
+            // Initialize the list of students
+            Students = new ObservableCollection<Student>();
+            Students.Add(new Student { Name = "Omar", Surname = "Belghaouti", StudentID = 1 });
+            Students.Add(new Student { Name = "Nail", Surname = "Kamilov", StudentID = 2 });
+            Students.Add(new Student { Name = "Jose David Lopez", Surname = "Piqueras", StudentID = 3 });
+            Students.Add(new Student { Name = "Borja Aran", Surname = "Tesapa", StudentID = 4 });
+            Students.Add(new Student { Name = "Ermek", Surname = "Ashirov", StudentID = 5 });
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
